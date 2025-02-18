259b2c10e1a9e7ca591b990a1a63823b5ef12caa
robsoncaliban <robsoncaliban@gmail.com>
2025-02-18 20:55:36 -0300

    Primeiro commit

---
+++ src/test/java/com/robsoncaliban/habit_flow_api/HabitFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsoncaliban.habit_flow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
