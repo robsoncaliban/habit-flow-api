64c4fb750b702fc14619146fbde6b7455d5b2cdf
robsoncaliban <robsoncaliban@gmail.com>
2025-02-20 18:02:34 -0300

    feat: Cria e mapeia entidade Usuario para o banco de dados

---
+++ src/main/java/com/robsoncaliban/habit_flow_api/entities/Usuario.java
@@ -0,0 +1,66 @@
+package com.robsoncaliban.habit_flow_api.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_USUARIO")
+public class Usuario implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, length = 30)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false, length = 15)
+    private String senha;
+    
+    public Usuario() {
+    }
+    
+    public Usuario(String nome, String senha, String email) {
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
